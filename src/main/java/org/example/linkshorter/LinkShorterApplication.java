eb6a658d0d7bb14b3f306140ca52725e1a1cdf81
Yura-Bulgakov <yura.bulgakov.00@gmail.com>
2024-02-27 12:03:58 +0300

    Initial commit

---
+++ src/main/java/org/example/linkshorter/LinkShorterApplication.java
@@ -0,0 +1,13 @@
+package org.example.linkshorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShorterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShorterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/linkshorter/LinkShorterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.linkshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
