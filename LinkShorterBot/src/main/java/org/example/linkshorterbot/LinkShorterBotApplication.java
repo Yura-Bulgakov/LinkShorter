f4d1de1737c6fdba907dae1dadfb698b028882b0
Yura-Bulgakov <yura.bulgakov.00@gmail.com>
2024-03-18 22:09:50 +0300

    Add bot module

---
+++ LinkShorterBot/src/main/java/org/example/linkshorterbot/LinkShorterBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.linkshorterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShorterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShorterBotApplication.class, args);
+    }
+
+}
---
+++ LinkShorterBot/src/test/java/org/example/linkshorterbot/LinkShorterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.linkshorterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShorterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
